8435ab86fbdd4607ff15db45d85d82fd49f539d5
Henrique Oliveira <henrique.oliveira.gomes@gmail.com>
2022-08-06 23:15:40 -0300

    First Go code

---
+++ helloWorld/hello/hello.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	names := []string{"Glayds", "Samantha", "Darrin"}
+
+	messages, err := greetings.Hellos(names)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get a greeting message and print it.
+	fmt.Println(messages)
+}
